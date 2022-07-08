772713973cd5c3467e903d6f0574d0ddb919cd63
Simone Taeggi <startae14@gmail.com>
2022-07-08 09:29:09 +0200

    Configuring SSO with Github

--- config/admin.js
+++ config/admin.js
@@ -1,6 +1,31 @@
+const GithubStrategy = require("passport-github2");
+
 module.exports = ({ env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET'),
+    providers: [
+      {
+        uid: "github",
+        displayName: "Github",
+        icon: "https://cdn1.iconfinder.com/data/icons/logotypes/32/github-512.png",
+        createStrategy: (strapi) =>
+          new GithubStrategy(
+            {
+              clientID: env("GITHUB_CLIENT_ID"),
+              clientSecret: env("GITHUB_CLIENT_SECRET"),
+              scope: ["user:email"],
+              callbackURL:
+                strapi.admin.services.passport.getStrategyCallbackURL("github"),
+            },
+            (accessToken, refreshToken, profile, done) => {
+              done(null, {
+                email: profile.emails[0].value,
+                username: profile.username,
+              });
+            }
+          ),
+      },
+    ],
   },
   apiToken: {
     salt: env('API_TOKEN_SALT'),
